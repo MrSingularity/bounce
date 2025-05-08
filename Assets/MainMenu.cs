a883cf592e13f021ad72dfe8f513715e1a158c07
gabriel gillmann <gabriel.gillmann@bluewin.ch>
2025-05-08 15:46:11 +0200

    added sound

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -1,16 +1,33 @@
 using UnityEngine;
 using System.Collections;
-using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public AudioSource clickSound;
+    public float delayBeforeAction = 0.5f;
+
     public void PlayGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        StartCoroutine(PlayGameWithDelay());
     }
+
     public void QuitGame()
     {
+        StartCoroutine(QuitGameWithDelay());
+    }
+
+    private IEnumerator PlayGameWithDelay()
+    {
+        if (clickSound != null) clickSound.Play();
+        yield return new WaitForSeconds(delayBeforeAction);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    private IEnumerator QuitGameWithDelay()
+    {
+        if (clickSound != null) clickSound.Play();
+        yield return new WaitForSeconds(delayBeforeAction);
         Debug.Log("Quit Game");
         Application.Quit();
     }
---
+++ Assets/TextMesh Pro/Examples & Extras/Scripts/PlaySound.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class PlaySound : MonoBehaviour
+{
+    public AudioSource audioSource;
+
+    public void PlayClickSound()
+    {
+        audioSource.Play();
+    }
+}
